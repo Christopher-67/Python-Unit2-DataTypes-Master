eaf8b6a03f690b9ac11b21d13efec22d8a67dbf3
Christopher-67 <christophers892@nycstudents.net>
2025-09-29 20:05:31 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,2 +1,12 @@
-GCF:
-if x %1 == 0 and y %2 == 0
+def gcf(x, y):
+    if x < y:
+        smaller = x
+    else: 
+        smaller = y
+    for i in range(1, smaller + 1):
+     if x % i == 0 and y % i == 0:
+        gcf = i
+    return(gcf)
+number1 = 9
+number2 = 48
+print("gcf of", number1, "and", number2, "is", gcf(number1, number2))
